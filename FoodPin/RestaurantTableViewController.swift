ed5cdc79cbccf6d5745492dab4a36ddc3dca2b5b
huyaye <huyaye@naver.com>
2016-12-28 12:39:09 +0900

    Introduction to Navigation Controller and Segue
    Passing Data Using Segues

--- FoodPin/RestaurantTableViewController.swift
+++ FoodPin/RestaurantTableViewController.swift
@@ -125,6 +125,15 @@ class RestaurantTableViewController: UITableViewController {
         return [deleteAction, shareAction]
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showRestaurantDetail" {
+            if let indexPath = tableView.indexPathForSelectedRow {
+                let destinationController = segue.destination as! RestaurantDetailViewController
+                destinationController.restaurantImage = restaurantImages[indexPath.row]
+            }
+        }
+    }
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
