a809fb9dd77b67e5ec7b72d5c33c319ab53314cc
huyaye <huyaye@naver.com>
2016-12-28 16:56:57 +0900

    Detail View Enhancement and Navigation Bar Customization
    Exercise #1 : Adding phone number to the detail view

--- FoodPin/RestaurantDetailViewController.swift
+++ FoodPin/RestaurantDetailViewController.swift
@@ -36,7 +36,7 @@ class RestaurantDetailViewController: UIViewController, UITableViewDataSource, U
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 4
+        return 5
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -53,6 +53,9 @@ class RestaurantDetailViewController: UIViewController, UITableViewDataSource, U
             cell.fieldLabel.text = "Location"
             cell.valueLabel.text = restaurant.location
         case 3:
+            cell.fieldLabel.text = "Phone"
+            cell.valueLabel.text = restaurant.phone
+        case 4:
             cell.fieldLabel.text = "Been here"
             cell.valueLabel.text = restaurant.isVisited ? "Yes, I've been here before" : "No"
         default:
