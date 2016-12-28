6116ea40bd5e365a00358be9f1589fc63d2f068b
huyaye <huyaye@naver.com>
2016-12-28 16:41:28 +0900

    Detail View Enhancement and Navigation Bar Customization
    Change the Style of Status Bar

--- FoodPin/AppDelegate.swift
+++ FoodPin/AppDelegate.swift
@@ -16,11 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // Customize Navigation bar
         UINavigationBar.appearance().barTintColor = UIColor(red: 216.0/255.0, green: 74.0/255.0, blue: 32.0/255.0, alpha: 1.0)
         UINavigationBar.appearance().tintColor = UIColor.white
         if let barFont = UIFont(name: "Avenir-Light", size: 24.0) {
             UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName:UIColor.white, NSFontAttributeName:barFont]
         }
+        // Customize Status bar
+        UIApplication.shared.statusBarStyle = .lightContent
         return true
     }
 
