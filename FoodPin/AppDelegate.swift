34b7be0df2de8e6e4854a6980bfd9e282ad49c48
huyaye <huyaye@naver.com>
2016-12-28 17:03:00 +0900

    Detail View Enhancement and Navigation Bar Customization
    Exercise #2 : Changing the font for the navigation bar

--- FoodPin/AppDelegate.swift
+++ FoodPin/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Customize Navigation bar
         UINavigationBar.appearance().barTintColor = UIColor(red: 216.0/255.0, green: 74.0/255.0, blue: 32.0/255.0, alpha: 1.0)
         UINavigationBar.appearance().tintColor = UIColor.white
-        if let barFont = UIFont(name: "Avenir-Light", size: 24.0) {
+        if let barFont = UIFont(name: "AvenirNext-DemiBold", size: 24.0) {
             UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName:UIColor.white, NSFontAttributeName:barFont]
         }
         // Customize Status bar
