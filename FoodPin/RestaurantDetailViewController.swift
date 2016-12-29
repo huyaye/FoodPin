90ce85237a49e1f947221dc68169effa686ee86c
huyaye <huyaye@naver.com>
2016-12-29 14:22:25 +0900

    Basic Animations, Visual Effects and Unwind Segues
    Unwind Segues and Data Passing

--- FoodPin/RestaurantDetailViewController.swift
+++ FoodPin/RestaurantDetailViewController.swift
@@ -61,7 +61,7 @@ class RestaurantDetailViewController: UIViewController, UITableViewDataSource, U
             cell.valueLabel.text = restaurant.phone
         case 4:
             cell.fieldLabel.text = "Been here"
-            cell.valueLabel.text = restaurant.isVisited ? "Yes, I've been here before" : "No"
+            cell.valueLabel.text = restaurant.isVisited ? "Yes, I've been here before. \(restaurant.rating)" : "No"
         default:
             cell.fieldLabel.text = ""
             cell.valueLabel.text = ""
@@ -72,6 +72,19 @@ class RestaurantDetailViewController: UIViewController, UITableViewDataSource, U
     
     @IBAction func close(segue:UIStoryboardSegue) {
         
+    }
+    
+    @IBAction func ratingButtonTapped(segue: UIStoryboardSegue) {
+        if let rating = segue.identifier {
+            restaurant.isVisited = true
+            switch rating {
+            case "great": restaurant.rating = "Absolutely love it! Must try."
+            case "good": restaurant.rating = "Pretty good."
+            case "dislike": restaurant.rating = "I don't like it."
+            default: break
+            }
+        }
+        tableView.reloadData()
     }
     
     /*
