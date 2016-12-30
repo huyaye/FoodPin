41e2158e54097b9cdd44346570b193c52eee1721
huyaye <huyaye@naver.com>
2016-12-30 14:32:21 +0900

    Introduction to Static Table Views, UIImagePickerController and NSLayoutConstraint
    Adding a new TableViewController to add new restaurant

--- FoodPin/RestaurantTableViewController.swift
+++ FoodPin/RestaurantTableViewController.swift
@@ -152,6 +152,10 @@ class RestaurantTableViewController: UITableViewController {
         }
     }
     
+    @IBAction func unwindToHomeScreen(segue:UIStoryboardSegue) {
+        
+    }
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
