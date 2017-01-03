390bc249c73359770ea1062ae4ff5b545aca1a2b
huyaye <huyaye@naver.com>
2017-01-03 23:15:26 +0900

    Building Walkthrough Screens with UIPageViewController
    Designing the Walkthrough Screens and ViewController

---
+++ FoodPin/WalkthroughContentViewController.swift
@@ -0,0 +1,45 @@
+//
+//  WalkthroughContentViewController.swift
+//  FoodPin
+//
+//  Created by 유정욱 on 2017. 1. 3..
+//  Copyright © 2017년 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class WalkthroughContentViewController: UIViewController {
+    @IBOutlet var headingLabel: UILabel!
+    @IBOutlet var contentLabel: UILabel!
+    @IBOutlet var contentImageView: UIImageView!
+    
+    var index = 0   // store the current page index
+    var heading = ""
+    var imageFile = ""
+    var content = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        headingLabel.text = heading
+        contentLabel.text = content
+        contentImageView.image = UIImage(named: imageFile)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
